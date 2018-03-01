a5b16539d142ea9794f8a9b520d5b77e7076476e
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-01 09:40:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-var api = "http://apihost.com:port";
+var api = "http://bkc.npool.pw:10040";
 
-var poolHost = "apihost.com";
+var poolHost = "bkc.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/coin";
+var blockExplorer = "http://blockexplorer.pw/bkc";
 
 var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
 
@@ -15,5 +15,7 @@ var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchai
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"],
+        ["npool.pw/bkc", "http://bkc.npool.pw:10040"],
+        ["miner.balkancoin.org", "http://miner.balkancoin.org:8147"],
+        ["bkc.mininggood.com", "http://us2.mininggood.com:8147"]
 ];
