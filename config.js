2ea89a60f9a35310cf369eca222777409e2c4498
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-10 14:11:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,5 +17,6 @@ var themeCss = "themes/default-theme.css";
 var networkStat = [
         ["npool.pw/bkc", "http://bkc.npool.pw:10040"],
         ["miner.balkancoin.org", "http://miner.balkancoin.org:8147"],
-        ["bkc.mininggood.com", "http://us2.mininggood.com:8147"]
+        ["bkc.mininggood.com", "http://us2.mininggood.com:8147"],
+        ["bkc.euminingpool.com", "http://bkc.euminingpool.com:8147"]
 ];
