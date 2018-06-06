22841c845257294f77f12b2bd4b4377713612a8d
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-06 09:28:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = "http://ca.npool.pw:3000";
+var api = "https://api.npool.pw/bkc";
 
 var poolHost = "ca.npool.pw";
 
@@ -15,7 +15,7 @@ var transactionExplorer = "http://explorer.balkancoin.org/?hash={id}#blockchain_
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["npool.pw/bkc", "http://ca.npool.pw:3000"],
+        ["npool.pw/bkc", "https://api.npool.pw/bkc"],
         ["miner.balkancoin.org", "http://miner.balkancoin.org:8147"],
         ["bkc.mininggood.com", "http://us2.mininggood.com:8147"],
         ["bkc.euminingpool.com", "http://bkc.euminingpool.com:8147"]
